9b6d535dd37f0534587546c6cd15f834bd7a64ae
Sena Isaka <YOUR_EMAIL_ADDRESS>
2025-08-21 13:59:43 +0900

    neovimのcoc-settingsの設定

--- neovim/link.sh
+++ neovim/link.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
 mkdir -p ~/.config/nvim && ln -sf ~/.dotfiles/neovim/init.vim ~/.config/nvim/init.vim
+
+ln -sf ~/.dotfiles/neovim/coc-settings.json ~/.config/nvim/coc-settings.json
+
 mkdir -p ~/.config/nvim/after/plugin && ln -sf ~/.dotfiles/neovim/after.vim ~/.config/nvim/after/plugin/unmaps.vim
 
